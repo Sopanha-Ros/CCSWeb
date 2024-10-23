f65cfbd4a3e3063528889ba8772f79fea815b188
sopanha <sopanharos08@gmail.com>
2024-10-23 15:43:22 +0700

    update web

--- mainapp/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.template import loader
-
-def mainapp(request):
-  template = loader.get_template('homepage.html')
-  return HttpResponse(template.render())
